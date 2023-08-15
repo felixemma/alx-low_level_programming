dac89a0057c5ea206d8f7abd3f52bf7ed489bd32
felixemma <felixemmanuel91@gmail.com>
2023-08-14 20:11:59 -0700

    sorry for q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+	(c <= 'z') {
+		if (( c != 'a' && c != 'e') && c <= 'z')
+			putchar(c);
+		c++;
+	}
+		putchar('\n');
+		return (0);
+}
