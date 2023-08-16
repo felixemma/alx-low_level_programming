7620e8d92f003a0b49fa2786bd30ce899e0cd59f
felixemma <felixemmanuel91@gmail.com>
2023-08-16 11:21:23 -0700

    0-putchar

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-0x02-functions_nested_loops
+this is the project readme
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
-
-void print_alphabet (void);
-
-void print_alphabet _×10(void);
-
-int _islower(int c); 
-
-int _isalpha(int c);
-
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int print_sign(int n);
-
 int _abs(int);
-
 int print_last_digit(int);
-
-void jack_bauer (void);
-
+void jack_bauer(void);
 void times_table(void);
-
 int add(int, int);
-
-void print to 98(int n);
-
-void print_times_table(int n);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
