62540615159bbaac1c1c9b723ac27d27da32d1fb
felixemma <felixemmanuel91@gmail.com>
2023-10-10 11:18:00 -0700

    more bit manipulation update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,7 +7,7 @@ int clear_bit(unsigned long int *n, unsigned int index);
 int _atoi(const char *s);
 int _putchar(char c);
 int get_endianness(void);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
 unsigned int binary_to_uint(const char *b);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 void print_binary(unsigned long int n);
 #endif
